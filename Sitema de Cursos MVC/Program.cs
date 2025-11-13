335b662984aa9930ade1667f96c93b7b83742bb1
kessiamilena <kessia.milena124@yahoo.com>
2025-11-13 08:27:54 -0300

    :ok_hand: Finalização da atividade, incluindo conexão com banco e Views

--- Sitema de Cursos MVC/Program.cs
+++ Sitema de Cursos MVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Sitema_de_Cursos_MVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 var app = builder.Build();
 
@@ -22,7 +28,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Curso}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
--- Sitema de Cursos MVC/obj/Debug/net9.0/Sitema de Cursos MVC.AssemblyInfo.cs
+++ Sitema de Cursos MVC/obj/Debug/net9.0/Sitema de Cursos MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sitema de Cursos MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ab17074b9956a7c3a3ca75199c59f6dd6aafb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sitema de Cursos MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sitema de Cursos MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
